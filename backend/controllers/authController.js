87dae320226a5901883ae14d034559717c349859
Hi 👋, I'm Mohammad Asim <muhammadasim4927@gmail.com>
2025-09-27 16:02:00 +0500

    feat(auth): add login API (on top of signup)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,6 +7,19 @@ const {body,validationResult}=require('express-validator')
 const nodemailer=require('nodemailer');
 const User=db.user
 
+const validationLogin = [
+  body("email_address")
+  .trim()
+  .notEmpty()
+  .withMessage("Email is required")
+  .isEmail()
+  .withMessage("Please enter a valid email")
+  .normalizeEmail(),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+];
 
 const validationRegistration = [
   body("full_name")
@@ -75,9 +88,69 @@ const signupUser=async(req,res)=>{
         }
       };
 
+      
+      const loginUser = async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({
+            success: false,
+            errors: errors.array(),
+          });
+        }
+
+      
+        try {
+          const { email_address, password } = req.body;
+      
+          const existingUser = await User.findOne({
+            where: { email_address }
+          });
+      
+          if (!existingUser) {
+            return res.status(404).json({
+              success: false,
+              message: "User not registered"
+            });
+          }
+      
+          const isMatch = await bcrypt.compare(password, existingUser.password);
+          if (!isMatch) {
+            return res.status(401).json({
+              success: false,
+              message: "Invalid credentials"
+            });
+          }
+      
+          const token = jwt.sign(
+            { id: existingUser.id, email: existingUser.email_address },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: '1h' }
+          );
+      
+          return res.status(200).json({
+            success: true,
+            message: "Login successful",
+            user: {
+              id: existingUser.id,
+              full_name: existingUser.full_name,
+              email_address: existingUser.email_address,
+            },
+            token,
+          });
+      
+        } catch (err) {
+          console.error("Login error:", err);
+          return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+          });
+        }
+      };
+
+
 
 
   
 
-module.exports={signupUser,
-  validationRegistration}
+module.exports={signupUser,loginUser,
+  validationRegistration,validationLogin}
